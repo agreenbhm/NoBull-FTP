72aea51f16b9e077fef41da65755f0dff5b99e43
Drew Green <agreenbhm@gmail.com>
2011-02-18 16:56:32 -0500

    Directory structure browseable.
    Signed-off-by: Drew Green <agreenbhm@gmail.com>

--- list_contents.php
+++ list_contents.php
@@ -7,16 +7,35 @@ if($conn_id = ftp_connect($_SESSION['ftp_server'], $_SESSION['ftp_port'], 30)) /
 
 	if(@ftp_login($conn_id, $_SESSION['ftp_user'], $_SESSION['ftp_pw'])) //If login to FTP server using cached credentials is successful
 	{	
-		echo "Logged in to FTP, list contents<br><br>";
-		$contents = ftp_nlist($conn_id, ".");
-		sort($contents);
+		
+		if(isset($_GET['ftp_parent']) && $_GET['ftp_parent'] == "true")  //If parameter passed to change directory
+		{
+			ftp_chdir($conn_id, $_GET['ftp_currentdir']);
+			ftp_chdir($conn_id, '../');
+			//ftp_cdup($conn_id); //Execute change
+		} //End change directory
+		
+		if(isset($_GET['ftp_chdir']))  //If parameter passed to change directory
+		{
+			$chdir = $_GET['ftp_chdir']; //Set $chdir to passed parameter
+			ftp_chdir($conn_id, $chdir); //Execute change
+		} //End change directory
+
+		echo "Logged in to FTP, list contents<br><br>"; //Diagnostic message
+		$current_dir = ftp_pwd($conn_id); //Set variable of current working directory
+		echo 'Current directory:  "' . $current_dir . '"<br><br>'; //Output current directory
+		
+		echo '<a href="./uploader.php?ftp_currentdir=' . $current_dir . '&ftp_parent=true">(Up One Level)</a><br>';
+		
+		$contents = ftp_nlist($conn_id, "."); //Get contents of FTP folder
+		sort($contents); //Sort FTP contents alphabetically
 		
 		foreach($contents as $i) //List remote folders
 		{
 			if(ftp_size($conn_id, $i) == -1)
 			{
-				$i = ltrim($i, "./");
-				echo '<a href="./chdir.php?ftp_chdir=' . $i . '">[ ' . $i . ' ]</a></font><br>';
+				$i = ltrim($i, "./"); //Remove leading ./ from name
+				echo '<a href="./uploader.php?ftp_chdir=' . $current_dir . '/' . $i . '">[ ' . $i . ' ]</a></font><br>'; //Output folders & link to chdir
 			}
 			
 		} //End list remote  folders
@@ -25,8 +44,8 @@ if($conn_id = ftp_connect($_SESSION['ftp_server'], $_SESSION['ftp_port'], 30)) /
 		{
 			if(ftp_size($conn_id, $i) !== -1)
 			{
-				$i = ltrim($i, "./");
-				echo '<a href="./chdir.php?ftp_get=' . $i . '">' . $i . '</a><br>';
+				$i = ltrim($i, "./"); //Remove  leading ./ from name
+				echo '<a href="./uploader.php?ftp_get=' . $i . '">' . $i . '</a><br>'; //Output files & link to fget
 			}
 		} //End list remote files		
 	} //End if able to login
